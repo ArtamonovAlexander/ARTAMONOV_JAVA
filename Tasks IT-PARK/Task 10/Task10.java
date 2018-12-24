f38c6a36f678fe554b804186ab96ee2e91790ce0
ArtamonovAlexander <darc@list.ru>
2018-12-24 17:45:38 +0300

    add Task10

---
+++ Tasks IT-PARK/Task 10/Task10.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Task10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char text1[] = scanner.nextLine().toCharArray();
+        char text2[] = scanner.nextLine().toCharArray();
+        int value1 = 0;
+        int value2 = 0;
+        int index = 0;
+
+        for (int i = 0; i < text1.length; i++) {
+            if (text1[i] == text2[0]) {
+                index = i;
+                for (int j = 0; j < text2.length; j++) {
+                    value1 = (int) text1[i + j];
+                    value2 = (int) text2[j];
+                }
+                if (value1 == value2) {
+                    System.out.println("Congratulations");
+                    System.out.println("Вхождение второй строки в первую начинается с индекса " + index);
+                }
+            }
+        }
+
+    }
+}
