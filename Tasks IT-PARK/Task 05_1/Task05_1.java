923062c5015ef92289b9227db0d603fd79bc539b
ArtamonovAlexander <darc@list.ru>
2019-01-08 14:28:21 +0300

    add findMin in Task05_1

--- Tasks IT-PARK/Task 05_1/Task05_1.java
+++ Tasks IT-PARK/Task 05_1/Task05_1.java
@@ -1,7 +1,9 @@
-public class Task05_1 {
+public class Task05 {
     public static void main(String[] args) {
         int array[] = {3, 5, 10, -1, 34, 50, 11};
         System.out.println(findMax(array, 0));
+        System.out.println(findMin(array, 0));
+
 
 //        int min = array[0];
 //        int max = array[0];
@@ -14,9 +16,22 @@ public class Task05_1 {
     public static int findMax(int[] array, int max) {
         if (array.length > max) {
             int next = findMax(array, max + 1);
+            System.out.println("next=" + next + " max=" + max);
             return (array[max] > next) ? array[max] : next;
         } else {
             return array[0];
         }
     }
+    public static int findMin(int[] array, int min) {
+        if (array.length > min) {
+            int next = findMin(array, min + 1);
+            System.out.println("next=" + next + " min=" + min);
+            return (array[min] < next) ? array[min] : next;
+        } else {
+            return array[0];
+        }
+    }
 }
+
+
+
