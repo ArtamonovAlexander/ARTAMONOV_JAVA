92b8be2bd532c9e9d86b3e669592387092291f7a
ArtamonovAlexander <darc@list.ru>
2019-01-14 16:58:32 +0300

    add amendment

--- Tasks IT-PARK/Task 06_1/Task06_1.java
+++ Tasks IT-PARK/Task 06_1/Task06_1.java
@@ -14,15 +14,14 @@ public class Task06_1 {
 //        }
     }
 
-    public static int redirect(int[] array, int index) {
-        if (array.length > index) {
-            redirect(array, index + 1);
-            if (array.length/2 > index) return array[index];
-            int tmp = array[array.length - index - 1];
-            array[array.length - index - 1] = array[index];
-            array[index] = tmp;
-            return array[0];
+     public static int[] redirect(int[] array) {
+        int min;
+
+        for (int i = 0; i < array.length / 2; i++) {
+            min = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = min;
         }
-        else return array[index-1];
+        return array;
     }
 }
--- Tasks IT-PARK/Task 10_1/Task10_1.java
+++ Tasks IT-PARK/Task 10_1/Task10_1.java
@@ -8,34 +8,21 @@ public class Task10 {
         char text1[] = scanner.nextLine().toCharArray();
         char text2[] = scanner.nextLine().toCharArray();
         equallyString(text1, text2, 0, 0 ,0);
-//        int value1 = 0;
-//        int value2 = 0;
-//
-//        for (int i = 0; i < text1.length; i++) {
-//            if (text1[i] == text2[0]) {
-//                for (int j = 0; j < text2.length; j++) {
-//                    value1 = (int) text1[i + j];
-//                    value2 = (int) text2[j];
-//                }
-//                if (value1 == value2) {
-//                    System.out.println("Congratulations");
-//                    System.out.println("¬хождение второй строки в первую начинаетс€ с индекса " + i);
-//                }
-//            }
-//        }
+
     }
 
     public static int equallyString(char[] text1, char[] text2, int index, int value1,int value2){
         if (index > text1.length) return index;
         equallyString(text1, text2, index + 1, value1,value2);
-        if (text1[text1.length - index] == text2[0]) {                                  // не нашел ошибку ArrayIndexOutOfBoundsException
+        if (text1[text1.length - index] == text2[0]) {                                  ArrayIndexOutOfBoundsException
             for (int j = 0; j < text2.length; j++) {
-                value1 = (int) text1[text1.length - index + j];
-                value2 = (int) text2[j];
+                value1 += (int) text1[text1.length - index + j];
+                value2 += (int) text2[j];
             }
             if (value1 == value2) {
                 System.out.println("Congratulations");
                 System.out.println("¬хождение второй строки в первую начинаетс€ с индекса " + (text1.length - index));
+		return;
             }
         }
         return text1.length - index;
