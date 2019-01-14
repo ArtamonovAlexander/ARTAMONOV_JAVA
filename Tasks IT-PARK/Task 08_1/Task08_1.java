6e2ad3c635fe66b027b758274ffe1f0289ec5f02
ArtamonovAlexander <darc@list.ru>
2019-01-14 17:09:50 +0300

    add amendment

--- Tasks IT-PARK/Task 08_1/Task08_1.java
+++ Tasks IT-PARK/Task 08_1/Task08_1.java
@@ -1,9 +1,12 @@
-public class Task08_1 {
+public class Task08 {
     public static void main(String[] args) {
         int array[] = {7, 5, 10, -1, 34, 50, 11};
-        sortChoise(array, 0);
-        for (int b : array) {  System.out.println(b);   }
+        sortChoise(array);
+        for (int b : array) {
+            System.out.print(b);
+        }
     }
+
 //                for (int i = 0; i < array.length - 1; i++) {
 //                    int min = i;
 //                    for (int j = i + 1; j < array.length; j++) {
@@ -21,10 +24,8 @@ public class Task08_1 {
 //        }
 
 
-    public static int[] sortChoise(int[] array, int index) {
-        if (index > array.length) return array;
-        sortChoise(array, index + 1);
-        for (int i = 0; i < index - 1; i++) {
+    public static int[] sortChoise(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
             int min = i;
             for (int j = i + 1; j < array.length; j++) {
                 if (array[j] < array[min]) {
