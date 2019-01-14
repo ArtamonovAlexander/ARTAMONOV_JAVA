c80c2e3c7cef959d6ae4607d2c4cef820fd0b888
ArtamonovAlexander <darc@list.ru>
2019-01-14 17:04:08 +0300

    add amendment

--- Tasks IT-PARK/Task 07_1/Task07_1.java
+++ Tasks IT-PARK/Task 07_1/Task07_1.java
@@ -1,7 +1,7 @@
-public class Task07 {
+public class Task07_1 {
     public static void main(String[] args) {
         int array[] = {7, 5, 10, -1, 34, 50, 11};
-        localMax(array, 0, 0);
+        System.out.println(localMax(array, 0));
 
 //        int k = 0;
 //        for (int i = 1; i < array.length - 1; i++) {
@@ -22,30 +22,23 @@ public class Task07 {
 //        System.out.println(k);
     }
 
-    public static int[] localMax(int[] array, int index, int k) {
-
-        if (array.length > index) {
-            localMax(array, index + 1, k);
-        }
-        if (index == 0) {
-            if (array[0] > array[1]) {
+    public static int localMax(int[] array, int k) {
+        for (int i = 1; i < array.length - 1; i++) {
+            if (i == 1) {
+                if (array[0] > array[1]) {
+                    k++;
+                }
+            }
+            if (i == array.length) {
+                if (array[array.length - 1] > array[array.length - 2]) {
+                    k++;
+                }
+            }
+            if (array[i] > array[i + 1] && array[i] > array[i - 1]) {
                 k++;
-                System.out.println(k);
             }
         }
-        if (index == array.length) {
-            if (array[array.length - 1] > array[array.length - 2]) {
-                k++;
-                System.out.println(k);
-            }
-        }
-        if ((index - 3) <= 0) return array;
-        if (array[index - 1] < array[index - 2] && array[index - 2] > array[index - 3]) {
-            k++;
-            System.out.println(k);
-        }
-
-        return array;
+        return k;
     }
 
 }
