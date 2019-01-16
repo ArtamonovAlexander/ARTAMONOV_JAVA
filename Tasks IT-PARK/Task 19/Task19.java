bf8aefad678f870b971d7ac5eb3394e5b3d5f37c
ArtamonovAlexander <darc@list.ru>
2019-01-16 18:28:57 +0300

    add Scanner in Task 19

--- Tasks IT-PARK/Task 19/Task19.java
+++ Tasks IT-PARK/Task 19/Task19.java
@@ -1,4 +1,28 @@
 
+import java.util.Scanner;
+
+public class Task19 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int array[][] = new int[4][4];
+        for (int i = 0; i < array.length; i++) {            //заполнение массива
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = scanner.nextInt();
+                if ((i + i) < (i + j)){
+                    array[i][j] *= 2;
+                }
+            }
+        }
+        for (int i = 0; i < array.length; i++) {            //вывод на экран
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
 public class Task19 {
     public static void main(String[] args) {
         int array[][] = new int[4][4];
